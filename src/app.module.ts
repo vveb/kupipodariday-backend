b24974b62028868425de701d2bd309410cfb186c
Victor Tarapata <vvteach@yandex.ru>
2024-05-29 17:05:20 +0300

    feat: init TypeOrm & create API framework

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { WishesModule } from './wishes/wishes.module';
+import { WishlistsModule } from './wishlists/wishlists.module';
+import { OffersModule } from './offers/offers.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'student',
+      password: 'student',
+      database: 'kupipodariday',
+      entities: [],
+      synchronize: true,
+    }),
+    UsersModule,
+    WishesModule,
+    WishlistsModule,
+    OffersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

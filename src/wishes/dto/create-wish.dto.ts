f086034f5b89b8544d44f2aaa6b2367e0b44200a
Victor Tarapata <vvteach@yandex.ru>
2024-06-02 20:06:15 +0300

    feat: add wish dto

--- src/wishes/dto/create-wish.dto.ts
+++ src/wishes/dto/create-wish.dto.ts
@@ -1,1 +1,23 @@
-export class CreateWishDto {}
+import { IsNumber, IsString, IsUrl, Length } from 'class-validator';
+
+export class CreateWishDto {
+  @IsString()
+  @Length(1, 250, { message: 'Имя подарка должно быть от 1 до 250 символов' })
+  name: string;
+
+  @IsUrl()
+  link: string;
+
+  @IsUrl()
+  image: string;
+
+  @IsNumber()
+  price: number;
+
+  @IsString()
+  @Length(1, 1024, {
+    message: 'Описание подарка должно быть от 1 до 1024 символов',
+  })
+  description: string;
+
+}

8b5dc73c02d044e8ac8c53501211d54bc0bdff80
Victor Tarapata <vvteach@yandex.ru>
2024-06-02 20:08:40 +0300

    feat: add wishlist dto

--- src/wishlists/dto/create-wishlist.dto.ts
+++ src/wishlists/dto/create-wishlist.dto.ts
@@ -1,1 +1,16 @@
-export class CreateWishlistDto {}
+import { IsUrl, Length, Max } from 'class-validator';
+
+export class CreateWishlistDto {
+  @Length(1, 250, {
+    message: 'Название списка должно быть от 1 до 250 символов',
+  })
+  name: string;
+
+  @Max(1500, {
+    message: 'Описание подборки не должно превышать 1 500 символов',
+  })
+  description: string;
+
+  @IsUrl()
+  image: string;
+}
